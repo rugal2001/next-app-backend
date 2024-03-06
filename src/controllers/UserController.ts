f807c4b6bbb03c5d42f4751bfd0863b8101fa1ce
ibrahim <karroumiibrahim33@gmail.com>
2024-03-06 18:12:33 +0100

    decode the token to extract the data of current user (error)

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -9,7 +9,6 @@ export const register = async (req: express.Request, res: express.Response) => {
     const { firstName, lastName, email, password, role } = req.body;
     const users: UserType[] = await UserModel.find();
     const userCheck = await users.find((u) => u.email === email);
-    console.log(userCheck);
     if (userCheck) {
         res.status(400).json({ message: "this email is already used" });
     }
@@ -40,6 +39,7 @@ export const login = async (req: express.Request, res: express.Response) => {
   const { email, password } = req.body;
   const users: UserType[] = await UserModel.find();
   const user = await users.find((u) => u.email === email);
+  
 
   if (!user) {
       return res.status(401).send("No User Found");
@@ -54,6 +54,20 @@ export const login = async (req: express.Request, res: express.Response) => {
   const token = jwt.sign({ userId: user.email }, secretKey, {
       expiresIn: "1h",
   });
+  const decodedToken = jwt.decode(token);
+  const userId = decodedToken.userId;
+  UserModel.findById(userId)
+  .then((user :any)=>{
+    if (!user) {
+      console.error('User not found');
+      return;
+    }
+    const userName: string = user.name;
+    console.log('User name:', userName);
+  })
+  .catch((err:Error)=>{
+    console.error('Error querying user:', err);
+  })
 
   
   res.status(200).json({access_token:token} );
