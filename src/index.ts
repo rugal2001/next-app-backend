3d93028c125aa29d472d58ad88b5197ebe7f9f2f
ibrahim <karroumiibrahim33@gmail.com>
2024-03-06 18:13:34 +0100

    .

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,9 @@ import express, { json, urlencoded } from "express";
 import mongoose from "mongoose";
 import router from "./routes";
 import cors from "cors";
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 // import { cloudinaryConfig ,uploader } from '../src/utils/cloudinary';
 // import {upload ,  from '../src/middleware/multer'
-
 
 const app = express();
 const port = 4000;
@@ -14,17 +13,14 @@ app.use(express.json());
 app.use(urlencoded({ extended: false }));
 app.use(json());
 
-
-
 app.use(cors());
 app.use("/", router);
-
 
 /////////////////////////////////////////////////////
 ////////////// DATABASE CONFIGURATION  //////////////
 const MONGO_URL = "mongodb://localhost:27017";
 mongoose
-.connect(MONGO_URL, {
+  .connect(MONGO_URL, {
     dbName: "mern-post",
   })
   .then(() => {
@@ -38,7 +34,3 @@ mongoose
 app.listen(port, () => {
   console.log(`listening in port http://localhost:${port} `);
 });
-
-
-
-
