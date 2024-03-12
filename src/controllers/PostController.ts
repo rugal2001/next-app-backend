6c80446ba32ff9c7ba8b8af8aa0ed4ab6a666ed7
ibrahim <karroumiibrahim33@gmail.com>
2024-03-12 14:08:02 +0000

    remove the upload image and add the update post and save the user who post that post

--- src/controllers/PostController.ts
+++ src/controllers/PostController.ts
@@ -1,7 +1,7 @@
 import express, { request } from "express";
-import { PostModel } from "../db/post";
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
+import { PostModel } from "../models/post";
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 //////////////////////////////////////////////////////
 export const getAllPosts = async (
@@ -9,7 +9,7 @@ export const getAllPosts = async (
   response: express.Response
 ) => {
   try {
-    const posts = await PostModel.find();
+    const posts = await PostModel.find().populate("user", "firstName lastName image");
     return response.status(200).json({ data: posts });
   } catch (error) {
     return response.status(400);
@@ -35,66 +35,44 @@ export const createPost = async (
   response: express.Response
 ) => {
   try {
-    const { name, contenue, image } = request.body;
+    const { name, contenue, image, user } = request.body;
     const post = new PostModel({
       name,
       contenue,
       image,
+      user
     });
     await post.save();
     return response.status(200).json({ message: "Post Created", data: post });
   } catch (error) {
-    console.log("there is no data to push")
+    console.log("there is no data to push");
     return response.status(400);
   }
 };
-/////////////////////////////////////////////////////////////////
-
-export const uploadImg = (req: express.Request, res: express.Response) => {
-  if (req.file) {
-    console.log({ r: req.file });
-    res.status(200).json( req.file.path );
-  } else {
-    return res.status(400).json({
-      message: "No file uploaded",
-    });
-  }
-};
-
-////////////////////////////////////////////////////////////////////////////
-
-
-
-////////////////////////////////////////////////////////////////////////////
- 
 
 
 /////////////////////////////////////////////////////////////////////////
-// export const updatePost = async (
-//   request: express.Request,
-//   response: express.Response
-// ) => {
-//   console.log("im in update function");
-//   try {
-//     const { id } = request.params;
-//     const { name, contenue } = request.body;
+export const updatePost = async (
+  request: express.Request,
+  response: express.Response
+) => {
+  console.log("im in update function");
+  try {
+    const { id } = request.params;
+    const { contenue } = request.body;
 
-//     const result = await cloudinary.uploader.upload(request.file?.path);
-//     const post = await PostModel.findById(id);
-//     if (post) {
-//       post.name = name;
-//       post.contenue = contenue;
-//       post.image = result.secure_url;
-//       console.log("this is result.secure_url", result.secure_url);
-//       await post.save();
-//     }
+    const post = await PostModel.findById(id);
+    if (post) {
+      post.contenue = contenue;
+      await post.save();
+    }
 
-//     return response.status(200).json({ message: "Post Updated", data: post });
-//   } catch (error) {
-//     return response.status(400);
-//   }
-// };
-
+    return response.status(200).json({ message: "Post Updated", data: post });
+  } catch (error) {
+    return response.status(400);
+  }
+};
+/////////////////////////////////////////////////////////////////////////////
 export const deletePost = async (
   request: express.Request,
   response: express.Response
