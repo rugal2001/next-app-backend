5596d289eff9f91c35542c8ae3bf247ec855f42a
ibrahim <karroumiibrahim33@gmail.com>
2024-03-12 14:09:18 +0000

    rename the folder to models and reference the user to the posts

---
+++ src/models/post.ts
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose';
+const PostSchema = new mongoose.Schema({
+    name : {
+        type : String,
+        require : true,
+    },
+    contenue : {
+        type : String,
+        require : true,
+    },
+    image :{
+        type : String,
+        require : true,
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'user', // Reference to the UserModel
+        required: true
+    }
+},{timestamps : true})
+
+export const PostModel = mongoose.model('post' , PostSchema);
