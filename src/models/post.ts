43108e3d29b23120329c10c2f81d2a5bad8c44be
ibrahim <karroumiibrahim33@gmail.com>
2024-03-22 16:41:16 +0000

    sSupport likes in posts schema

--- src/models/post.ts
+++ src/models/post.ts
@@ -1,22 +1,29 @@
-import mongoose from 'mongoose';
-const PostSchema = new mongoose.Schema({
-    name : {
-        type : String,
-        require : true,
+import mongoose from "mongoose";
+const PostSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      require: true,
     },
-    contenue : {
-        type : String,
-        require : true,
+    contenue: {
+      type: String,
+      require: true,
     },
-    image :{
-        type : String,
-        require : true,
+    image: {
+      type: String,
+      require: true,
+    },
+    likeCount: {
+      type: Number,
+      default: 0,
     },
     user: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'user', // Reference to the UserModel
-        required: true
-    }
-},{timestamps : true})
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "user", // Reference to the UserModel
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
 
-export const PostModel = mongoose.model('post' , PostSchema);
+export const PostModel = mongoose.model("post", PostSchema);
