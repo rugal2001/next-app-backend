ce185f1e74d856c678267fc203f6ab69443ccb37
ibrahim <karroumiibrahim33@gmail.com>
2024-03-28 10:51:41 +0000

    Add try catch to the upload image

--- src/controllers/ImageController.ts
+++ src/controllers/ImageController.ts
@@ -1,10 +1,17 @@
 import express from "express";
+
 export const uploadImg = (req: express.Request, res: express.Response) => {
-  if (req.file) {
-    res.status(200).json(req.file.path);
-  } else {
-    return res.status(400).json({
-      message: "No file uploaded",
-    });
+  try {
+    if (req.file) {
+      const filePath = req.file.path;
+      res.status(200).json({ filePath });
+    } else {
+      return res.status(400).json({
+        message: "No file uploaded",
+      });
+    }
+  } catch (error) {
+    console.error("Error uploading image:", error);
+    return res.status(500).json({ message: "Internal server error" });
   }
 };
